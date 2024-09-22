5409b2fc930aa9ae2c3a30f60ac77f38f9fd617c
Manish kasera <66238224+Manish-kasera@users.noreply.github.com>
2024-09-22 17:11:54 +0530

    Create Business Problem Standup.sql

---
+++ Business Problem Standup.sql
@@ -0,0 +1,17 @@
+-- Here are the 15 business problem from Indian Standup Comedy Dataset
+
+1.Retrieve the names of all comedians(channel_name) in the dataset and name must be in Asceding Order.
+2.Find the total number of videos uploaded by each comedian(channel_name) and sort From highest total video to lowest .
+3.List the channel_name ,titles of videos,number of views that have more than 10,0000 views and sort highest to lowest.
+4.Retrieve the videos published in 2022.  
+5. Find the total number of likes for a all comedian's sort by highest like to lowest .
+6. List comedians who have uploaded more than 100 videos sort from highest total_number of videos to lowest.
+7.Show the titles of videos with a view count higher than the average view count of all videos.
+8. Retrieve the channel_name,video titles and like counts of the top 3 videos with the highest like-to-view ratio.
+9. List all videos published in the 2021 and order them by the number of views in desc.
+10. Identify comedians who have more than 20 videos with over 1 million views.Sort them by total count more than 1 million view.
+11. Which comedian and its video title has the highest like-to-view ratio for their most recent video, and what is the ratio?
+12. Identify comedians who have a higher total comment count in 2021 compared to 2020.
+13.For each comedian, calculate the average number of likes per video and compare it with the overall average of all comedians. List those who exceed the overall average.
+14.Find the comedian who consistently gained more views over time (i.e., whose videos have an increasing view count trend over successive uploads).
+15.Determine the top 5 comedians who have shown the greatest improvement in like-to-view ratio from their earliest to latest video.  
